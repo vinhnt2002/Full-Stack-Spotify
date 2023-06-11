e8ac7cf056549a4de5cdd9ea8a28992e1d55d1f8
vinh nguyễn <vinhntse2002@gmail.com>
2023-06-11 13:51:44 +0700

    complete layout

--- next13-spotify/components/ListItem.tsx
+++ next13-spotify/components/ListItem.tsx
@@ -1,28 +1,25 @@
-"use client"
+"use client";
 
 import Image from "next/image";
+import { FaPlay } from "react-icons/fa";
 import { useRouter } from "next/navigation";
-
-interface ListItemProps{
-    image: string,
-    name: string,
-    href: string
+interface ListItemProps {
+  image: string;
+  name: string;
+  href: string;
 }
 
-const ListItem: React.FC<ListItemProps> = ({
-    image,
-    name,
-    href
-}) => {
-    const router = useRouter()
+const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
+  const router = useRouter();
 
-    const onClick = () =>{
-
-        // add authentication before push
-        router.push(href)
-    }
-  return <button
-  className="
+  const onClick = () => {
+    // add authentication before push
+    router.push(href);
+  };
+  return (
+    <button
+      onClick={onClick}
+      className="
   relative
   group
   flex
@@ -35,21 +32,37 @@ const ListItem: React.FC<ListItemProps> = ({
   transition
   pr-4
   "
-  >
-    <div className="
+    >
+      <div
+        className="
     relative
     min-h-[64px]
     min-w-[64px]
-    ">
-        <Image
-        className="object-cover"
-        src={image}
-        fill
-        alt="Image"
-        />
-    </div>
-    <p className="font-medium truncate py-5">{name}</p>
-  </button>
+    "
+      >
+        <Image className="object-cover" src={image} fill alt="Image" />
+      </div>
+      <p className="font-medium truncate py-5">{name}</p>
+      <div
+        className="
+    absolute
+    transition
+    opacity-0
+    flex
+    rounded-full
+    items-center
+    bg-green-500
+    p-4
+    drop-shadow-md
+    right-5
+    group-hover:opacity-100
+    hover:scale-110
+    "
+      >
+        <FaPlay className="text-black" />
+      </div>
+    </button>
+  );
 };
 
 export default ListItem;
